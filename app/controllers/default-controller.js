451898861bc48e3f20d66e708309b49ff52ec5a3
kumaran-14 <kumarangowrisankar@yahoo.in>
2018-08-07 19:57:22 +0530

    basic setup

---
+++ app/controllers/default-controller.js
@@ -0,0 +1,7 @@
+const signale = require('signale')
+const schema = require('../models/defaultSchema.js')
+
+
+exports.dosomething = (req,res) => {
+
+}
---
+++ app/models/defaultSchema.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const defaultSchema = mongoose.Schema({
+
+})
+
+module.exports = mongoose.model('collection name', defaultSchema)

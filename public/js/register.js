8ca9ee6ed7339902ac9a3bd4fa9372eabb2ad9d3
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-08-11 16:35:56 +0530

    Add Document Uploads section and bugs fixed

--- public/js/register.js
+++ public/js/register.js
@@ -1,6 +1,7 @@
-const part_1 = document.querySelector('.part-1')
-const part_2 = document.querySelector('.part-2')
-const main =  document.querySelector('.main')
+const part_1 = document.querySelector('.part-1');
+const part_2 = document.querySelector('.part-2');
+const part_3 = document.querySelector('.part-3');
+const main =  document.querySelector('.main');
 
 main.addEventListener('click',hideContent);
 
@@ -14,6 +15,10 @@ function hideContent(e){
                     e.target.classList.toggle('fa-caret-up');
                     break;
         case '2':   part_2.classList.toggle("hide")
+                    e.target.classList.toggle('fa-caret-down');
+                    e.target.classList.toggle('fa-caret-up');
+                    break;
+        case '3':   part_3.classList.toggle("hide")
                     e.target.classList.toggle('fa-caret-down');
                     e.target.classList.toggle('fa-caret-up');
                     break;

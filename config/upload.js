051ee20cc42c5befbc97075f2421b5f92a556faa
nimish <nimishagarwal76@gmail.com>
2018-09-09 13:24:02 +0530

    linked portals

--- config/upload.js
+++ config/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 /** Storage Engine */
 const storageEngine = multer.diskStorage({
-  destination: './public/files/uploads/',
+  destination: '../Scholarship-Portal-Admin/public/files/uploads',
   filename: function(req, file, fn) {
     fn(null, new Date().getTime().toString() + '-' + file.fieldname + path.extname(file.originalname));
   }

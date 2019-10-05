982f21175b8bbd48bae3bcfdf7fe1d2c2f99237b
Bharath Kumar Ravichandran <bharathkumarravichandran@gmail.com>
2019-10-05 11:43:26 +0530

    [config] Add config.example.js

---
+++ config/config.example.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mongodb: {
+      dbURI: "mongodb://localhost:27017/scholarshipportal2019"
+    },
+    session: {
+      secretString: ""
+    },
+    dir: {
+      BASE_DIR: "",
+      ADMIN_BASE_DIR: ""
+    }
+  };
